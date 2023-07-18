ae03ce4bf72eeb3a03c181d136b62e5241e2dc80
Rogério dos Santos <rogercodeeti@gmail.com>
2023-07-18 09:30:26 -0300

    Adicionado o arquivo de cadasto.java

---
+++ cadastro.java
@@ -0,0 +1,16 @@
+package projagenda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Autor: Rogério dos Santos Objetivo: Projeto Agenda telefonica
+ */
+public class ProjAgenda {
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+       
+    }
+
+}
