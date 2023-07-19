c3fa429a5be090d1445127868412b241bb3973ff
Rogério dos Santos <rogercodeeti@gmail.com>
2023-07-19 09:18:57 -0300

    Adicionado uma linha de código

--- cadastro.java
+++ cadastro.java
@@ -10,6 +10,7 @@ public class ProjAgenda {
 
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        System.out.println("Tela principal");
        
     }
 
